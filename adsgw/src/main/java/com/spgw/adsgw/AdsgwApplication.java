5a3a4c68b4a5b657d53f56378d92cf24420af785
Shahzad R <shahzad.riasat@gmail.com>
2023-10-01 22:59:35 -0500

    gw

---
+++ adsgw/src/main/java/com/spgw/adsgw/AdsgwApplication.java
@@ -0,0 +1,13 @@
+package com.spgw.adsgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsgwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsgwApplication.class, args);
+	}
+
+}
---
+++ adsgw/src/test/java/com/spgw/adsgw/AdsgwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spgw.adsgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsgwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
